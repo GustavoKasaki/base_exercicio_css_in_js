3ea731c9ad3abd8ebeb8f0cb3bb584103a0398f6
GustavoKasaki <gmkasaki@gmail.com>
2024-12-20 15:51:37 -0300

    updated Hero style

--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   :root {
@@ -17,17 +17,15 @@ const GlobalStyle = createGlobalStyle`
   body {
     padding-bottom: 120px;
   }
+`
 
-  .container {
-    max-width: 1024px;
-    width: 100%;
-    margin: 0 auto;
-  }
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
 
   @media (max-width: 1024px) {
-    .container {
-      max-width: 80%;
-    }
+    max-width: 80%;
   }
 `
 
